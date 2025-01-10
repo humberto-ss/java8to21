d3f071786991e9e50a4b2948664dd6dda193bd5d
humberto-ss <humberto@MSI>
2025-01-10 12:19:52 +0100

    Collection Factories

--- src/main/java/java9/collection_factory/After.java
+++ src/main/java/java9/collection_factory/After.java
@@ -1,4 +1,84 @@
 package java9.collection_factory;
 
+import jdk.jfr.Description;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+
 public class After {
+
+
+    //------List Examples ------
+    @Test(expected = UnsupportedOperationException.class)
+    public void normalList(){
+        List<String> normalList = List.of("It", "is","a","normal","List");
+        printWithSpace(normalList);
+        normalList.set(3,"Unmodifiable");
+    }
+
+    @Test//(expected = NullPointerException.class)
+    public void unmodifiableListWithNull_shouldThrowNullPointer(){
+        Assert.assertThrows(NullPointerException.class,
+                () ->{
+                    List.of("It", "is","a","unmodifiable",null,"List");
+                });
+    }
+
+    //----- Set Examples -----------
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void normalSet(){
+
+
+        Set<String> normalSet = Set.of( "It", "is","a","normal","Set");
+        printWithSpace(normalSet);
+
+        Set<String> temp = new LinkedHashSet<>(Arrays.asList("It", "is","a","Linked","Set"));
+        Set<String> copySet = Set.copyOf( temp);
+        printWithSpace(copySet);
+
+        normalSet.remove("normal");
+        normalSet.add("Unmodifiable");
+
+    }
+    @Test(expected = NullPointerException.class)
+    public void unmodifiableSetWithNull_shouldThrowNullPointerException(){
+        Set<String> nullSet = Set.of("It", "is","a","normal",null,"Set");
+    }
+
+    //----- Map examples -------------
+    @Test(expected = UnsupportedOperationException.class)
+    public void normalMap() {
+        Map<String, String> tempMap = Map.of(
+                                            "MT", "Malta",
+                                            "BR", "Brazil",
+                                            "AR", "Argentina"
+                                            );
+
+        printWithSpace(tempMap);
+
+        tempMap.remove("AR");
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void unmodifiableMapWithNull_shouldThrowNullPointerException() {
+        Map<String, String> tempMap = Map.of(
+                                         "MT", "Malta",
+                                         "BR", "Brazil",
+                                         "AR", null
+                                            );
+        printWithSpace(tempMap);
+    }
+
+    private static void printWithSpace(Object obj){
+        System.out.println();
+        switch(obj){
+            case List l -> l.forEach(a -> System.out.print(a + " "));
+            case Set s -> s.forEach(a ->System.out.print(a+" "));
+            case Map m -> m.forEach((a,b) -> System.out.println(a+" is Key of "+b));
+            default -> new Object();
+        }
+    }
 }
--- src/main/java/java9/collection_factory/Before.java
+++ src/main/java/java9/collection_factory/Before.java
@@ -1,4 +1,85 @@
 package java9.collection_factory;
 
+import java.util.*;
+
 public class Before {
+
+    public static void main(String[] args) {
+//        normalList();
+//        unmodifiableListWithNull();
+
+//        normalSet();
+//        unmodifiableSetWithNull();
+
+        normalMap();
+        unmodifiableMapWithNull();
+    }
+
+    //------List Examples ------
+
+    static void normalList(){
+        List<String> normalList = Arrays.asList("It", "is","a","normal","List");
+        printWithSpace(normalList);
+
+        normalList.set(3,"Unmodifiable");
+        normalList = Collections.unmodifiableList(normalList);
+
+        printWithSpace(normalList);
+    }
+
+   static void unmodifiableListWithNull(){
+        List<String> listExample = Collections.unmodifiableList(Arrays.asList("It", "is","a","unmodifiable",null,"List"));
+        printWithSpace(listExample);
+    }
+
+    //----- Set Examples -----------
+
+    static void normalSet(){
+        Set<String> normalSet = new LinkedHashSet<>(Arrays.asList("It", "is","a","normal","Set"));
+        printWithSpace(normalSet);
+
+        normalSet.remove("normal");
+        normalSet.add("Unmodifiable");
+
+        normalSet = Collections.unmodifiableSet(normalSet);
+        printWithSpace(normalSet);
+
+    }
+    static void unmodifiableSetWithNull(){
+        Set<String> nullSet = Collections.unmodifiableSet(new HashSet<>(Arrays.asList("It", "is","a","normal",null,"Set")));
+        printWithSpace(nullSet);
+    }
+
+    //----- Map examples -------------
+    static void normalMap() {
+        Map<String, String> tempMap = new LinkedHashMap<>();
+        tempMap.put("MT", "Malta");
+        tempMap.put("BR", "Brazil");
+        tempMap.put("AR", "Argentina");
+        printWithSpace(tempMap);
+
+        tempMap.remove("AR");
+        printWithSpace(Collections.unmodifiableMap(tempMap));
+    }
+
+    static void unmodifiableMapWithNull() {
+        Map<String, String> tempMap = new LinkedHashMap<>();
+        tempMap.put("MT", "Malta");
+        tempMap.put("BR", "Brazil");
+        tempMap.put("AR", "Argentina");
+        printWithSpace(tempMap);
+
+        tempMap.replace("AR", null);
+        printWithSpace(Collections.unmodifiableMap(tempMap));
+    }
+
+    private static void printWithSpace(Object obj){
+        System.out.println();
+        switch(obj){
+            case List l -> l.forEach(a -> System.out.print(a + " "));
+            case Set s -> s.forEach(a ->System.out.print(a+" "));
+            case Map m -> m.forEach((a,b) -> System.out.println(a+" is Key of "+b));
+            default -> new Object();
+        }
+    }
 }
